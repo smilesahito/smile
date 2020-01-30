d524c5fc1f1392de72909d7500b32074b1b17e12
smilesahito <k181303@nu.edu.pk>
2020-01-30 12:04:57 +0500

    new file

---
+++ test1.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+ 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<div>
+
+<ul>
+  <li>
+    <p>This insurance shall not insure to the benefit of the carrier or other bailee.</p>
+  </li>
+  <li>
+    <p>It is a condition of this insurance that the assured shall act with reasonable dispatch in all circumstances within their control.</p>
+  </li>
+  <li>
+    <p>Where, after attachment of this insurance, the destination is changed by the assured, held covered at a premium and on condition to be arranged subject to prompt notice being given to the underwriters. </p>
+  </li>
+  <li>
+    <p>Excluding Seepage and Pollution as per Seepage and Pollution exclusion Clause </p>
+  </li>
+  <li>
+    <p>Warranted that all cargo to be containerized and professionally packed</p>
+  </li>
+  <li>
+    <p>Warranted Truck/Trailer to be covered with Tarpaulin if un-containerized </p>
+  </li>
+  <li>
+    <p>Terrorism Exclusion Clause </p>
+  </li>
+   <li>
+    <p>Warranted Packing, Stowage and Handling to be as per manufacturer’s Instructions. </p>
+  </li>
+   <li>
+    <p>Excluding any shortages found in sealed containers (with the seal intact) </p>
+  </li>
+   <li>
+    <p>Excluding mysterious and unexplainable disappearance. </p>
+  </li>
+   <li>
+    <p>Excluding pre-shipment damages</p>
+  </li>
+   <li>
+    <p>Excluding Transit to FATA & PATA</p>
+  </li>
+   <li>
+    <p>Subject to Carrier’s Breach of Trust Exclusion Clause </p>
+  </li>
+   <li>
+    <p>Warranted that the carry vehicle should not be left unattended at any stage of Transit.</p>
+  </li>
+   <li>
+    <p>In case of theft, Pilferage, non-delivery &/Or damage of goods the Insured during transit must lodge FIR / Claim to be lodged immediately with the relevant police authorities.</p>
+  </li>
+   <li>
+    <p>Warranted Truck not to be loaded beyond its capacity.</p>
+  </li>
+   <li>
+    <p>Transportation of Heavy Machinery for e.g. Chillers / Heavy Generators should be on low bed carriers. </p>
+  </li>
+  <li>
+    <p>Warranty that no loss will be paid on breach of any warranty/condition. </p>
+  </li>
+  <li>
+    <p>Subject to Rule 58 </p>
+  </li>
+  <li>
+    <p>Subject to Premium Payment Clause. </p>
+  </li>
+  <li>
+    <p>All other terms, conditions, limitations and warranties as per Policy Schedule. </p>
+  </li>
+  <li>
+    <p>Special conditions, warranties to be set depending on nature of consignment on case to case basis.</p>
+  </li>
+
+
+</ul>
+
+</div>
+
+
+
+</body>
+</html>
